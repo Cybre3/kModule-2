391c06982096455081b6954f67760c84da15bba7
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2020-12-18 00:25:34 -0500

    added fuelMoney lab for lesson 2

--- kingsLessons_M2/kM2Lesson2_12-15-20_Tuesday/LAB5_Data Types and Variables_12-17-20/LAB5_6.fuelMoney.js
+++ kingsLessons_M2/kM2Lesson2_12-15-20_Tuesday/LAB5_Data Types and Variables_12-17-20/LAB5_6.fuelMoney.js
@@ -1,6 +1,17 @@
 function main(distance, passengers, pricePerLiter) {
    
-    let neededFuel = (distance / 100) * 7;
+    // let emptyBusFuel = (distance / 100) * 7;
+    // let fuelConsumption = passengers * 0.1;
+    // let busOccupyFuel = emptyBusFuel + fuelConsumption;
+    // let neededMoney = busOccupyFuel * pricePerLiter;
 
-    console.log(`Needed money for that trip is ${neededMoney} lv`);
+    let tripFuel = (distance / 100) * 7;
+    tripFuel += passengers * 0.1;
+    let neededMoney = tripFuel * pricePerLiter;
+
+
+    console.log(`Needed money for that trip is ${neededMoney}lv.`);
 }
+
+main(260, 9, 2.49);
+main(90, 14, 2.88);
