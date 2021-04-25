38ec418c9934d25ad829b0a85bd8e25c6a18e75e
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-04-25 11:38:03 -0400

    Update LAB26_2.js

--- kingsLessons_M2/kM2Lesson9_01-28-21_Thurs/LAB26_Regular Expressions_01-30-21/LAB26_2.js
+++ kingsLessons_M2/kM2Lesson9_01-28-21_Thurs/LAB26_Regular Expressions_01-30-21/LAB26_2.js
@@ -2,7 +2,7 @@ function main(nums) {
     let pattern = /\+404(-| )\d{3}\1\d{4}\b/g;
 
     let matches = nums.match(pattern);
-    console.log(matches.join(' '));
+    console.log(matches.join(', '));
 }
 
 main('+404 222 2222,404-222-2222, +404/222/2222, +404-222 2222 +404 222-2222, +404-222-222, +404-222-22222 +404-222-2222');
